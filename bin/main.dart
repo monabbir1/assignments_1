cd154e21a41dce0b7ce193f5f9839b482da0bdb5
Monabbir <monabbirossain11@gmail.com>
2025-01-05 22:19:31 +0600

    ignored

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,3 @@
-main() async{
-  print("Stared");
- await timer();
- print("End");
-}
-timer() async {
- await Future.delayed(Duration(seconds: 3),(){
-    print("Run after 3 sec");
-  });
+main(){
+  print("Hello");
 }
